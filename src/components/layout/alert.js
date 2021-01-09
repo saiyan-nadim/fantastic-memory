8fce69172096fc834d00fbc4aba7bf32bdd79fbf
Nadim <nsaiyan45@gmail.com>
2021-01-09 18:46:19 +0600

    first commit

---
+++ src/components/layout/alert.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ const alert = ({ alert }) => {
+    
+        return (
+            alert !== null && (
+                <div className={`alert alert-${alert.type}`}>
+                <i className='fas fa-info-circle' /> {alert.msg}               
+                </div>
+            )
+          
+        )
+    }
+
+
+export default alert
